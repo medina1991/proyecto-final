ff5d4005856610af7b4597932c284420ba15aa5b
manrique <manrique@andres>
2014-11-17 20:36:43 -0500

    Ingresando Archivos de Presentacion, Inicio del Sistema Hay elementos a Modificar

---
+++ head.php
@@ -0,0 +1,25 @@
+		<div class="span12">
+				<div class="header">
+				<div class="pull-left">
+				<img class="stilogo" src="imagenes/Banner1.png"> 
+				</div>
+				</div>
+			
+
+					<div class="alert alert-info">
+						<Strong>(C) Aspirantes UN, </strong>&nbsp;Bienvenidos ! gracias por seguirnos...
+							
+							<div class="pull-right">
+								<i class="icon-calendar icon-large"></i>
+								<?php
+								$FechaHoy = date('y:m:d');
+								$new = date('l, F d, Y', strtotime($FechaHoy));
+								echo $new;
+								?>
+							</div>
+
+
+					</div>
+				
+					
+				</div>
